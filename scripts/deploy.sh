cdf01b66f6857ff3dda20382c1a8a70dcb60b6d6
Ntui Etta <92643385+Big-Zaza@users.noreply.github.com>
2023-10-02 10:13:16 +0100

    Update deploy.sh 22

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt install sshpass openssh-client
 
-sshpass -p "labers" scp -r /src/Average.php labers@192.168.2.18:/home/labers
+sshpass -p "team4" scp -r ./src/Average.php ubuntu@52.87.204.29:/home/ubuntu
 
 
