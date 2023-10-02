7a26682920a1482d15e6b802a9df642f08f56434
Ntui Etta <92643385+Big-Zaza@users.noreply.github.com>
2023-10-02 11:33:43 +0100

    Update deploy.sh 4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,13 @@
 sudo apt update
 sudo apt install sshpass openssh-client
 
-sshpass -p "team4" scp -r ./src/Average.php ubuntu@52.87.204.29:/home/ubuntu
+# Define the servers as an array
+servers=("user@server1" "user@server2")
+file_path="${{ github.workspace }}/$LOCAL_FILE"
+
+# Loop through the servers and send the file
+for server in "${servers[@]}"; do
+  sshpass -p "$LINUX_PASS" scp -r "$file_path" "$server:REMOTE_PATH"
+done
 
 
